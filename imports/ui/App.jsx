fc8ae919ed9c1bba3c99218fa082ab40ebb77dd1
John Aguilar <42220961+johnagl@users.noreply.github.com>
2019-06-07 14:28:36 -0700

    Job stage category

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Hello from './components/Hello.jsx';
 import Info from './components/Info.jsx';
-import JobStageCategory from './components/JobStageCategory.jsx'
+import JobStageCategory from './components/JobStageCategory.jsx';
+import JobStageCategoryDropDown from './components/JobStageCategoryDropDown.jsx';
+import AddJobPopup from './components/AddJobPopup.jsx';
+import AddJobForm from './components/AddJobForm.jsx'
 
 const App = () => (
   <div>
---
+++ imports/ui/components/JobStageCategoryDropDown.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class JobStageCategoryDropDown extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>JobStageCategoryDropDown</h1>
+
+      </div>
+    );
+  }
+}
