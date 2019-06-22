d09589953a04f6df7b7ef09fea03feaadace06aa
John Aguilar <42220961+johnagl@users.noreply.github.com>
2019-06-21 17:00:01 -0700

    fixed actions, added styling

--- imports/ui/components/JobStageCard.jsx
+++ imports/ui/components/JobStageCard.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { Card, Button, CardTitle, CardText } from 'reactstrap';
 import './JobStageCard.css';
 import AddButtonModal from './AddButtonModal.jsx';
+import JobStageDropdown from './JobStageDropdown.jsx'
 
 export default class JobStageCard extends Component {
     render() {
         return (
             <div className="jobStageCardStyle">
                 <Card body inverse style={{ backgroundColor: '#eaeaea', borderColor: '#eaeaea' }}>
-                    <CardTitle  className="jobStageText">Shortlist</CardTitle>
+                    {/* <CardTitle  className="jobStageText">Shortlist</CardTitle> */}
+                    <JobStageDropdown style={{margin: 'auto'}} />
                     <CardText></CardText>
                     <AddButtonModal />
                 </Card>
