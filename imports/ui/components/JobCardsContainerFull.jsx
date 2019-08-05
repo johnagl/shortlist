ce827b6af8e97524a8e3f43db8a78eddde447bc6
John Aguilar <42220961+johnagl@users.noreply.github.com>
2019-08-05 13:14:39 -0700

    Update JobCardsContainerFull.jsx

--- imports/ui/components/JobCardsContainerFull.jsx
+++ imports/ui/components/JobCardsContainerFull.jsx
@@ -37,7 +37,7 @@ class JobCardsContainerFull extends Component {
 
             return (
                 <div key={stage._id} className='columnStyle'>
-                    {stage.title == 'Shortlist' ?  
+                    {stage.title == 'Interested' ?  
                     <React.Fragment>
                         <JobStageCardFull stage={stage} jobs={jobs}/>
                         <JobCardsContainer stage={stage} jobs={jobs} direction={this.props.direction}/>
